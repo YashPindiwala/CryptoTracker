46dbe1b6ee9dd03eeacd31f1f3bdee8ba303d74a
izrvnar <izrvnar@gmail.com>
2023-04-04 08:11:24 -0400

    Added View Pager * Added View pager2 tp the application * Along with 5 different education pages * Added section to the bottom menu * Added Depth transformer to the view pager

--- app/src/main/java/com/project/app/cryptotracker/Dashboard/EducationFragment.java
+++ app/src/main/java/com/project/app/cryptotracker/Dashboard/EducationFragment.java
@@ -8,6 +8,8 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.project.app.cryptotracker.R;
 
@@ -23,9 +25,14 @@ public class EducationFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    private static final int ARG_PARAM3 = 0;
+
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private int mParam3;
 
     public EducationFragment() {
         // Required empty public constructor
@@ -40,11 +47,12 @@ public class EducationFragment extends Fragment {
      * @return A new instance of fragment EducationFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static EducationFragment newInstance(String param1, String param2) {
+    public static EducationFragment newInstance(String param1, String param2, int mParam3) {
         EducationFragment fragment = new EducationFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
+        args.putInt("index", mParam3);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,6 +63,7 @@ public class EducationFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam3 = getArguments().getInt("index", ARG_PARAM3);
         }
     }
 
@@ -63,7 +72,20 @@ public class EducationFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_education, container, false);
-        ViewPager2 education = view.findViewById(R.id.educationViewpager);
+
+        if(mParam1 != null){
+            TextView textView = view.findViewById(R.id.topic_title);
+            textView.setText(mParam1);
+        }
+
+        if(mParam2 != null){
+            TextView textView = view.findViewById(R.id.topic_description);
+            textView.setText(mParam2);
+        }
+
+        ImageView imageView = view.findViewById(R.id.topic_image);
+        imageView.setImageResource(mParam3);
+
         return view;
     }
 }
